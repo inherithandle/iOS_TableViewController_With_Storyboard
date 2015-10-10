b12e35847dd2d35f8fd254170eb14d51edb84683
최규태 <hello@choegyutaes-MacBook-Pro.local>
2015-10-10 21:00:29 +0900

    Initial Commit

---
+++ Ratings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by 최규태 on 10/10/15.
+//  Copyright (c) 2015 최규태. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
